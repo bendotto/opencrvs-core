3f66e0fd9b07301ab9afef2aa1169e973edbcf4c
Sahriar Toufiq <sahriartoufiq@gmail.com>
2019-06-14 19:36:39 +0600

    RadioButton design fixes

--- packages/components/src/components/interface/RadioButton.tsx
+++ packages/components/src/components/interface/RadioButton.tsx
@@ -1,55 +1,36 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.li`
-  padding-top: 5px;
-  padding-bottom: 5px;
+const Wrapper = styled.div`
   list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  width: auto;
+  align-items: center;
 `
 
 const Label = styled.label`
-  position: relative;
-  left: 10px;
-  top: -5px;
   color: ${({ theme }) => theme.colors.copy};
   ${({ theme }) => theme.fonts.bodyBoldStyle};
   cursor: pointer;
+  margin-left: 8px;
 `
 
 const Check = styled.span`
-  display: inline-block;
-  background: ${({ theme }) => theme.colors.copy};
+  display: flex;
+  justify-content: center;
+  border: 2px solid ${({ theme }) => theme.colors.copy};
+  height: 25px;
+  width: 25px;
   border-radius: 50%;
-  height: 22px;
-  width: 22px;
-  transition: border 0.25s linear;
-  -webkit-transition: border 0.25s linear;
-  z-index: 1;
-
-  &::after {
-    display: block;
-    position: relative;
-    content: '';
-    background: ${({ theme }) => theme.colors.white};
+  align-items: center;
+  & > span {
+    display: flex;
+    height: 13px;
+    width: 13px;
     border-radius: 50%;
-    height: 12px;
-    width: 12px;
-    top: -13px;
-    left: 5px;
-    transition: background 0.25s linear;
-    -webkit-transition: background 0.25s linear;
-  }
-
-  &::before {
-    display: block;
-    position: relative;
-    content: '';
     background: ${({ theme }) => theme.colors.white};
-    border-radius: 50%;
-    height: 18px;
-    width: 18px;
-    top: 2px;
-    left: 2px;
+    align-self: center;
     transition: background 0.25s linear;
     -webkit-transition: background 0.25s linear;
   }
@@ -57,13 +38,11 @@ const Check = styled.span`
 
 const Input = styled.input`
   position: absolute;
-  width: 16px;
-  height: 16px;
   opacity: 0;
   z-index: 2;
   cursor: pointer;
   /* stylelint-disable */
-  &:checked ~ ${Check}::after {
+  &:checked ~ ${Check} > span {
     /* stylelint-enable */
     background: ${({ theme }) => theme.colors.copy};
   }
@@ -97,7 +76,9 @@ export class RadioButton extends React.Component<IRadioButton> {
           value={value.toString()}
           onChange={this.onChange}
         />
-        <Check />
+        <Check>
+          <span />
+        </Check>
         <Label htmlFor={id}>{label}</Label>
       </Wrapper>
     )
